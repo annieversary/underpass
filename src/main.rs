d01d5df5c5e1345f367dca81724aede59ea9a034
annieversary <annie@versary.town>
2023-11-09 03:50:55 +0000

    add logging

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use backtrace::Backtrace;
+use std::path::PathBuf;
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 mod graph;
 mod nominatim;
@@ -20,6 +24,11 @@ async fn main() {
     if let Err(err @ dotenv::Error::LineParse(..)) = dotenv::dotenv() {
         panic!("{:?}", err);
     }
+
+    let log_path = std::env::var("LOG_PATH")
+        .expect("failed to get LOG_PATH")
+        .into();
+    let _guard1 = setup_tracing(log_path);
 
     if !Path::new("./public/taginfo.json").exists() {
         if let Err(err) = taginfo::update_taginfo().await {
@@ -81,4 +90,44 @@ async fn js() -> ([(&'static str, &'static str); 1], String) {
     let a = include_str!("../public/index.js").to_string();
 
     ([("content-type", "text/js")], a)
+}
+
+fn setup_tracing(log_path: PathBuf) -> WorkerGuard {
+    std::panic::set_hook(Box::new(|panic| {
+        let b = Backtrace::new();
+        if let Some(location) = panic.location() {
+            tracing::error!(
+                message = %panic,
+                panic.file = location.file(),
+                panic.line = location.line(),
+                panic.column = location.column(),
+                backtrace = ?b,
+            );
+        } else {
+            tracing::error!(message = %panic, backtrace = ?b);
+        }
+    }));
+
+    let log_filter = EnvFilter::from_env("LOG_LEVEL");
+    #[allow(unused_variables)]
+    let error_filter = EnvFilter::from_env("ERROR_LEVEL");
+
+    // normal logging
+    let t = tracing_subscriber::registry().with(log_filter);
+
+    // file
+    let file_appender = tracing_appender::rolling::daily(&log_path, "app.log");
+    #[allow(unused_variables)]
+    let (non_blocking, guard1) = tracing_appender::non_blocking(file_appender);
+    #[cfg(not(debug_assertions))]
+    let t = t.with(tracing_subscriber::fmt::layer().with_writer(non_blocking));
+    // stdout
+    #[cfg(debug_assertions)]
+    let t = t.with(
+        tracing_subscriber::fmt::layer().with_writer(std::io::stdout), // .with_filter(log_filter),
+    );
+
+    t.init();
+
+    guard1
 }

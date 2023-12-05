873b5a1437e2c338b658fa52db7500b04ad2b70b
annieversary <annie@versary.town>
2023-12-05 03:30:43 +0000

    clippy

--- src/main.rs
+++ src/main.rs
@@ -116,7 +116,7 @@ fn setup_tracing(log_path: PathBuf) -> WorkerGuard {
     let t = tracing_subscriber::registry().with(log_filter);
 
     // file
-    let file_appender = tracing_appender::rolling::daily(&log_path, "app.log");
+    let file_appender = tracing_appender::rolling::daily(log_path, "app.log");
     #[allow(unused_variables)]
     let (non_blocking, guard1) = tracing_appender::non_blocking(file_appender);
     #[cfg(not(debug_assertions))]

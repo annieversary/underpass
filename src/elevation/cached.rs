37c0285471c591383bfaaa9f18070c786acf1c0c
annieversary <annie@versary.town>
2024-10-01 17:35:29 -0500

    fix CachedGdalElevationMap

--- src/elevation/cached.rs
+++ src/elevation/cached.rs
@@ -2,22 +2,23 @@ use std::{path::PathBuf, sync::Arc};
 
 use ahash::HashMap;
 use gdal::Dataset;
+use tokio::sync::Mutex;
 
 use super::{
     gdal::{lookup_in_dataset, GdalElevationMap},
     ElevationError,
 };
 
-pub(super) struct CachedGdalElevationMap {
+pub struct CachedGdalElevationMap {
     map: GdalElevationMap,
-    // cache: HashMap<PathBuf, Arc<Dataset>>,
+    cache: Arc<Mutex<HashMap<PathBuf, Arc<Mutex<Dataset>>>>>,
 }
 
 impl CachedGdalElevationMap {
     pub(super) fn new(map: GdalElevationMap) -> Self {
         Self {
             map,
-            // cache: Default::default(),
+            cache: Default::default(),
         }
     }
 
@@ -25,15 +26,24 @@ impl CachedGdalElevationMap {
     pub(super) async fn lookup(&self, lng: f64, lat: f64) -> Result<i32, ElevationError> {
         let path = self.map.dataset_path(lng, lat)?;
 
-        let data = Dataset::open(path)?;
-        lookup_in_dataset(&data, lng, lat)
-        // if let Some(data) = self.cache.get(path) {
-        // } else {
-        //     let data = Dataset::open(path)?;
-        //     let r = lookup_in_dataset(&data, lng, lat);
-        //     // self.cache.insert(path.to_path_buf(), data);
+        let data = {
+            let cache = self.cache.lock().await;
+            cache.get(path).cloned()
+        };
 
-        //     r
-        // }
+        if let Some(data) = data {
+            let data = data.lock().await;
+            lookup_in_dataset(&data, lng, lat)
+        } else {
+            let data = Dataset::open(path)?;
+            let r = lookup_in_dataset(&data, lng, lat);
+
+            {
+                let mut cache = self.cache.lock().await;
+                cache.insert(path.to_path_buf(), Arc::new(Mutex::new(data)));
+            }
+
+            r
+        }
     }
 }
